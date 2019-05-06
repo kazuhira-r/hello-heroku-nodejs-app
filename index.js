c1ff2b6a8b846e0e19a720f40217371944f97266
kazuhira-r <kazuhira.m@gmail.com>
2019-05-06 22:36:59 +0900

    edit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello!!'));
+app.get('/', (req, res) => res.send('Hello Node.js App!!'));
 
 app.listen(port, () => console.log(`[${new Date()}] Server startup, listen port = ${port}`));
