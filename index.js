908ad00925e2e5496552b2342ba5dc1427868d48
kazuhira-r <kazuhira.m@gmail.com>
2019-05-06 23:54:42 +0900

    edit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello Node.js App!!'));
-app.get('/config/var', (req, res) => res.send(process.env.MESSAGE));
+app.get('/config/var', (req, res) => res.send(process.env.VAR_MESSAGE));
 app.get('/config/sensitive', (req, res) => res.send(process.env.SENSITIVE_MESSAGE));
 
 app.listen(port, () => console.log(`[${new Date()}] Server startup, listen port = ${port}`));
